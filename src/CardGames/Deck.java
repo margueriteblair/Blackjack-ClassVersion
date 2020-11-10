f7ada5fbf0ce482798b34f0542c4fad2a3d091eb
Marguerite Blair <marguerite.blair@gmail.com>
2020-11-09 20:42:33 -0500

    nested loop to create a full deck of cards

--- src/CardGames/Deck.java
+++ src/CardGames/Deck.java
@@ -1,8 +1,21 @@
 package CardGames;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Deck {
     private final static String[] SUITS = {}; //SEARCCH UNICODE FOR THE SUITES
     //THIS EXISTS MARGIE DONT FORGET
-    private final static int[] values = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13};
+    private final static int[] VALUES = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
     //values
+    private List<Card> cards;
+
+    public Deck() {
+        cards = new ArrayList<>();
+        for (var suit : SUITS) {
+            for (var value : VALUES) {
+                cards.add(new Card(value, suit));
+            }
+        }
+    }
 }
