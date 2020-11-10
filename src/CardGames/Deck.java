d0dd678515872a819231388044775227d7f7fbd5
Marguerite Blair <marguerite.blair@gmail.com>
2020-11-09 20:40:07 -0500

    using unicode to get the images of the suits

---
+++ src/CardGames/Deck.java
@@ -0,0 +1,8 @@
+package CardGames;
+
+public class Deck {
+    private final static String[] SUITS = {}; //SEARCCH UNICODE FOR THE SUITES
+    //THIS EXISTS MARGIE DONT FORGET
+    private final static int[] values = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13};
+    //values
+}
