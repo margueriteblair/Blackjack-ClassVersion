08b2e67a59fa14801b513ba5f10d9b6a1358dce5
Marguerite Blair <marguerite.blair@gmail.com>
2020-11-09 20:32:11 -0500

    basic card class with value and suit as attributes

---
+++ src/CardGames/Card.java
@@ -0,0 +1,11 @@
+package CardGames;
+
+public class Card {
+    private int value;
+    private String suit;
+
+    public Card(int value, String suit) {
+        this.value = value;
+        this.suit = suit;
+    }
+}
--- src/CardGames/Main.java
+++ src/CardGames/Main.java
@@ -1,9 +1,7 @@
-package com.margieblair;
+package CardGames;
 
 public class Main {
-
     public static void main(String[] args) {
-	// write your code here
 
     }
 }
